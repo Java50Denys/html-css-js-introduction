de90e74c3a9fcf07ff5df96d0dc5c45b08e331c1
DenisBug <zhuchenko.denys@gmail.com>
2023-02-04 15:56:31 +0200

    HW 19

--- app.js
+++ app.js
@@ -117,11 +117,23 @@ function getStringOccurrences(strings) {
  displayStringOccurrences(strings)
  //HW #19
 function getMostPopulatedCountry(employees) {
-    //TODO 
     //returns country with most amount of employees
+    const countries = getCountriesSortedByOccurrences(employees);
+    return countries[0];
 }
 function getMostPupulatedCountries(employees, counter) {
     //returns a given number (conter) of countries with most amount of employees
+    const countries = getCountriesSortedByOccurrences(employees);
+    return countries.slice(0, counter);
+
+}
+function getCountriesSortedByOccurrences(employees) {
+    const countries = employees.map(e => e.address.country);
+    const countriesOccurrences = getStringsOccurrences(countries);
+    const arrayOccurrences = Object.entries(countriesOccurrences);
+    arrayOccurrences.sort(stringComp);
+    return arrayOccurrences.map(entry => entry[0]);
+
 }
 function isAnagram(word, anagram) {
     //TODO 
@@ -131,4 +143,23 @@ function isAnagram(word, anagram) {
     //hello anagram examples: elolh, olleh, ohell, lehol
     //exampls non-anagrams: eloll (no h), ollehh(different length),
     // olaeh ("a" doesn't exist in word), oleh(different length)
+    let res = false;
+    if (word.length === anagram.length) {
+        const letters = Array.from(word);
+    const lettersOccurrences = getStringsOccurrences(letters);
+    const anagramLetters = Array.from(anagram);
+   res = anagramLetters.every(letter => {
+        let res = false;
+        if (lettersOccurrences[letter]) {
+            res = true;
+            lettersOccurrences[letter]--;
+            if(lettersOccurrences[letter] == 0) {
+                delete lettersOccurrences[letter];
+            }
+        }
+        return res;
+    })
+    }
+    return res;
+
 }
